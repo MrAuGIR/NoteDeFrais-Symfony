da5eb9b8c73319036ea2636d2dc29d8357966267
MrAuGir <augirard17@gmail.com>
2021-05-08 10:16:58 +0200

    vehicle

---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\VehicleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VehicleRepository::class)
+ */
+#[ApiResource]
+class Vehicle
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $imma;
+
+    /**
+     * @ORM\Column(type="string", length=155, nullable=true)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $totalDistance;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $distanceCurrYear;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $DistanceLastYear;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImma(): ?string
+    {
+        return $this->imma;
+    }
+
+    public function setImma(string $imma): self
+    {
+        $this->imma = $imma;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(?string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getTotalDistance(): ?int
+    {
+        return $this->totalDistance;
+    }
+
+    public function setTotalDistance(?int $totalDistance): self
+    {
+        $this->totalDistance = $totalDistance;
+
+        return $this;
+    }
+
+    public function getDistanceCurrYear(): ?int
+    {
+        return $this->distanceCurrYear;
+    }
+
+    public function setDistanceCurrYear(?int $distanceCurrYear): self
+    {
+        $this->distanceCurrYear = $distanceCurrYear;
+
+        return $this;
+    }
+
+    public function getDistanceLastYear(): ?int
+    {
+        return $this->DistanceLastYear;
+    }
+
+    public function setDistanceLastYear(?int $DistanceLastYear): self
+    {
+        $this->DistanceLastYear = $DistanceLastYear;
+
+        return $this;
+    }
+}
