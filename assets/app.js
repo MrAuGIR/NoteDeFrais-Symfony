71ad3912ca93f7a7907fc097909f4b29f2d83d42
MrAuGir <augirard17@gmail.com>
2021-05-16 15:39:53 +0200

    reacr install + navbar

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,25 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+//import de react
+import React from 'react';
+import ReactDom from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import NavBar from './js/components/NavBar';
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
-// start the Stimulus application
-import './bootstrap';
+
+
+const App = () =>{
+    return(
+         
+        <HashRouter>
+            <NavBar />
+        </HashRouter>
+    )
+}
+
+const rootElement = document.querySelector("#App");
+ReactDom.render(<App />, rootElement);
